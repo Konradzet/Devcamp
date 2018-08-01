217506c770731fe75db8a444d1438a0dd00ae81c
Konrad Zdzieba <konrad.zdzieba@croscomsoftware.com>
2018-08-01 23:52:09 +0200

    Created seeds file with sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My blogpost #{blog}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut porttitor tellus. Nam accumsan mollis felis vel imperdiet. Donec aliquam metus ac lectus consequat tempus. Fusce vel enim ac leo ultrices dapibus quis eu leo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce id ex quis lorem porttitor bibendum. Morbi sed auctor sem. Phasellus accumsan nisl ut sem consequat gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque tellus purus, aliquet non turpis et, sodales pulvinar urna. Duis in metus interdum, vulputate magna sodales, bibendum urna."
+  )
+end
+
+puts "10 blogs created"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )    
+end
+
+puts "s skills created"
+
+9.times do |item|
+  Portfolio.create!(
+      title: "Portfolio title #{item}",
+      subtitle: "My great service",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut porttitor tellus. Nam accumsan mollis felis vel imperdiet. Donec aliquam metus ac lectus consequat tempus. Fusce vel enim ac leo ultrices dapibus quis eu leo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce id ex quis lorem porttitor bibendum. Morbi sed auctor sem. Phasellus accumsan nisl ut sem consequat gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque tellus purus, aliquet non turpis et, sodales pulvinar urna. Duis in metus interdum, vulputate magna sodales, bibendum urna.",
+      main_image: "http://via.placeholder.com/600x400",
+      thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
